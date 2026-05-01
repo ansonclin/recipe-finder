7ec5d4f3c6674d6a180135fa9de78ff1eca1101b
Anson Lin <204535653+ansonclin@users.noreply.github.com>
2026-04-30 22:03:30 -0700

    Phase 3 : claude analyze endpoint

--- server/index.js
+++ server/index.js
@@ -3,25 +3,49 @@ const axios = require('axios');
 const Anthropic = require('@anthropic-ai/sdk');
 const anthropic = new Anthropic();
 const express = require('express');
+const { YoutubeTranscript } = require('youtube-transcript')
 const app = express();
 
 app.get('/api/hello', (req,res) => {
     res.json( {message:'Hello from the backend!'});
 });
 
-app.get('/api/search', async (req, res) => {
-  const ingredients = req.query.ingredients;
-  const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
-    params: {
-      key: process.env.YOUTUBE_API_KEY,
-      q: `${ingredients} recipe`,
-      part: 'snippet',
-      type: 'video',
-      maxResults: 5
-    }
+app.get('/api/analyze', async (req, res) => {
+  const title = req.query.title;
+  const description = req.query.description;
+  const videoId = req.query.videoId;
+
+  let context = description;
+
+  try {
+    const transcript = await YoutubeTranscript.fetchTranscript(videoId);
+    const transcriptText = transcript.map(t => t.text).join(' ');
+    context = transcriptText.slice(0, 2000);
+  } catch (e) {
+    context = description;
+  }
+
+  const message = await anthropic.messages.create({
+    model: 'claude-haiku-4-5-20251001',
+    max_tokens: 300,
+    messages: [{
+      role: 'user',
+      content: `Based on this YouTube recipe video, estimate the macros and give a 1-2 sentence summary. Also provide the ingridients for each recipe.
+      Title: ${title}
+      Content: ${context}
+      
+      Reply in this format:
+      Summary: ...
+      Calories: ...
+      Protein: ...
+      Carbs: ...
+      Fat: ...`
+    }]
   });
-  res.json(response.data.items);
+
+  res.json({ analysis: message.content[0].text });
 });
+
 
 app.get('/api/analyze', async (req, res) => {
   const title = req.query.title;
